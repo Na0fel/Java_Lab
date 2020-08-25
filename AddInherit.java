bb77c4368afa46ce5e0a9bce4c9c86be7be45222
Na0fel <naofelazahar@gmail.com>
2020-08-25 14:51:11 +0600

    First commit

---
+++ AddInherit.java
@@ -0,0 +1,21 @@
+class Arithmetic
+{
+public int add(int a, int b)
+{
+    return a+b;
+}
+   }
+
+   class Adder extends Arithmetic
+    {
+        }
+    public class AddInherit{
+ public static void main(String []args){
+
+     Adder a = new Adder();
+
+     System.out.println("My Superclass is: " + a.getClass().getSuperclass().getName());
+
+     System.out.print(a.add(10,32) + " " + a.add(10,3) + " " + a.add(10,10) + "\n");
+  }
+}
